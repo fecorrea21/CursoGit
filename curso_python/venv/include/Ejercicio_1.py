4e5b51c0be59359a1228a403278270cc69af6cff
fecorrea <fcorrea@omnibnk.com>
2019-06-07 17:47:09 -0500

    Prueba

---
+++ curso_python/venv/include/Ejercicio_1.py
@@ -0,0 +1,12 @@
+print ("Cual es tu Nombre: ")
+
+nombre = input()
+
+print("Cual es tu edad:")
+
+edad = input()
+
+
+print("Hola",nombre,"como estas?, tu edad es,",edad)
+
+print("Chao",nombre, "nos vemos otro día")
